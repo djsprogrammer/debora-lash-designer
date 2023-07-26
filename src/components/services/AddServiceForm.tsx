8c5c6106164383e87d856280a8ae59e06d72e4c1
Deivid Silva <imdeividsilva@gmail.com>
2023-07-26 10:36:54 -0300

    reiniciando o valor dos input automaticamente

--- src/components/services/AddServiceForm.tsx
+++ src/components/services/AddServiceForm.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useState, useRef } from 'react'
 import { SetServices } from '../../types/services'
 
 interface Props {
@@ -10,16 +10,23 @@ const AddServiceForm = ({ setServices }: Props) => {
     const [name, setName] = useState('')
     const [value, setValue] = useState('')
 
+    const nameInput = useRef<HTMLInputElement>(null)
+    const valueInput = useRef<HTMLInputElement>(null)
+
     const addService = (e: React.FormEvent<HTMLFormElement>) => {
         e.preventDefault()
         const newService = { name, value }
         setServices(services => [...services, newService])
+        if (nameInput.current && valueInput.current) {
+            nameInput.current.value = ''
+            valueInput.current.value = ''
+        }
     }
 
     return (
         <form className='d-flex justify-content-center' onSubmit={e => addService(e)}>
-            <input className='text-center' onChange={e => setName(e.target.value)} type='text' placeholder='Nome' required />
-            <input className='mx-2 text-center' onChange={e => setValue(e.target.value)} type='text' placeholder='Valor' required />
+            <input ref={nameInput} className='text-center' onChange={e => setName(e.target.value)} type='text' placeholder='Nome' required />
+            <input ref={valueInput} className='mx-2 text-center' onChange={e => setValue(e.target.value)} type='text' placeholder='Valor' required />
             <button className='btn btn-dark' type='submit'>Adicionar Serviço</button>
         </form>
     )
