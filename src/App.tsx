6a035990f1902a06ef9c8ffd93f12f34e907682f
Deivid Silva <imdeividsilva@gmail.com>
2023-08-06 15:16:43 -0300

    criação da função getDataFromServer

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import './App.css'
 import { Services as TServices } from './types/services'
@@ -9,6 +9,19 @@ import ServicesProvider from './ServicesContext'
 const Index = () => {
 
     const [services, setServices] = useState<TServices>([])
+
+    const getDataFromServer = (res: Response) => {
+        res.json().then((services: TServices) => {
+            if (services[0]) setServices(services)
+        })
+    }
+
+    useEffect(() => {
+        fetch('http://localhost:8080/all-services')
+            .then(res => {
+                if (res.status === 200) getDataFromServer(res)
+            })
+    })
 
     return (
         <div>
