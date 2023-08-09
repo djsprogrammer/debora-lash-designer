b1e8ceb0e378ba417603e997337e4f6fce190365
Deivid Silva <imdeividsilva@gmail.com>
2023-08-09 15:50:33 -0300

    ajustando tamanho e espaçamento da logo

--- src/fixedComponents/Header.tsx
+++ src/fixedComponents/Header.tsx
@@ -1,8 +1,8 @@
 const Index = () => {
 
     return (
-        <header className='text-bg-dark py-2 px-3'>
-            <h4 className='logo pt-1'>Débora Bento</h4>
+        <header className='text-bg-dark py-1 px-2'>
+            <h5 className='logo pt-1'>Débora Bento</h5>
         </header>
     )
 
