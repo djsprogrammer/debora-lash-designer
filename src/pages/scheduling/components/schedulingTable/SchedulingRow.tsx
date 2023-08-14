0d845c4c2d4c59539962205b1a394edf4602188e
Deivid Silva <imdeividsilva@gmail.com>
2023-08-14 14:16:41 -0300

    estilização do botão confirmar

--- src/pages/scheduling/components/schedulingTable/SchedulingRow.tsx
+++ src/pages/scheduling/components/schedulingTable/SchedulingRow.tsx
@@ -1,3 +1,4 @@
+import { useRef, useState, useEffect } from 'react'
 import { parseISO, format } from 'date-fns'
 import { ServiceScheduling } from '../../../../types/services'
 
@@ -8,6 +9,38 @@ interface Props {
 
 const Index = ({ scheduling, getScheduling }: Props) => {
 
+    const confirmButton = useRef<HTMLButtonElement>(null)
+
+    const [confirmed, setConfirmed] = useState(scheduling.confirmed)
+
+    useEffect(() => {
+
+        if (confirmButton.current) {
+
+            const button = confirmButton.current
+
+            if (confirmed) {
+
+                // Mudando o estilo do botão para indicar que foi confirmado
+                button.classList.add('btn-success')
+                button.classList.remove('btn-outline-success')
+
+                button.innerText = 'Confirmado'
+
+            } else {
+
+                // Mudando o estilo do botão para indicar que foi confirmado
+                button.classList.add('btn-outline-success')
+                button.classList.remove('btn-success')
+
+                button.innerText = 'Confirmar'                
+
+            }
+
+        }
+
+    }, [confirmed])
+
     const value = new Intl.NumberFormat('pt-BR', { style: 'currency', currency: 'BRL' }).format(Number(scheduling.service.value)).replace('R$', '')
     const date = format(parseISO(scheduling.date), 'dd/MM')
 
@@ -16,7 +49,10 @@ const Index = ({ scheduling, getScheduling }: Props) => {
             <td>{scheduling.client}</td>
             <td>{scheduling.service.name} -{value}</td>
             <td>{date}</td>
-            <td><button onClick={() => getScheduling(scheduling)}>Confirmar</button></td>
+            <td><button ref={confirmButton} className='btn btn-sm btn-outline-success' onClick={() => {
+                getScheduling(scheduling)
+                setConfirmed(!confirmed)
+            }}>Confirmar</button></td>
         </tr>
     )
 }
