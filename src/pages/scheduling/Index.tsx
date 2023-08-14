e00078bdc92898fd55c355b49633f42a96c5b0d6
Deivid Silva <imdeividsilva@gmail.com>
2023-08-14 13:20:53 -0300

    criação do SchedulingRow

--- src/pages/scheduling/Index.tsx
+++ src/pages/scheduling/Index.tsx
@@ -1,5 +1,5 @@
 import { useEffect } from 'react'
-import SchedulingTable from './components/SchedulingTable'
+import SchedulingTable from './components/schedulingTable/Index'
 import AnySchedulingAdvice from '../components/AnyAdvice'
 import AddSchedulingForm from './components/AddSchedulingForm'
 import { ServiceScheduling } from '../../types/services'
