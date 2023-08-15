71b55892a4096ee52100af025bae4b5cfba7220a
Deivid Silva <imdeividsilva@gmail.com>
2023-08-15 14:21:32 -0300

    setando schedulings de teste

--- src/pages/scheduling/Index.tsx
+++ src/pages/scheduling/Index.tsx
@@ -2,7 +2,7 @@ import { useState, useEffect } from 'react'
 import SchedulingTable from './components/schedulingTable/Index'
 import AnySchedulingAdvice from '../components/AnyAdvice'
 import AddSchedulingForm from './components/AddSchedulingForm'
-import { ServiceScheduling } from '../../types/services'
+import { ServiceScheduling, schedulingsSimulation } from '../../types/services'
 import { container } from '../../commonStyles'
 
 interface Props {
@@ -14,14 +14,18 @@ const Index = ({ setCurrentPage }: Props) => {
 	const [servicesScheduling, setServicesScheduling] = useState<ServiceScheduling[]>([])
 
 	useEffect(() => {
+		setServicesScheduling(schedulingsSimulation)
+	}, [])
+
+	useEffect(() => {
 		setCurrentPage(1)
 	}, [setCurrentPage])
 
 	return (
 		<div className={container}>
-			{servicesScheduling[0] 
-			? <SchedulingTable servicesScheduling={servicesScheduling} setServicesScheduling={setServicesScheduling} />
-			: <AnySchedulingAdvice page='agendamento' />}
+			{servicesScheduling[0]
+				? <SchedulingTable servicesScheduling={servicesScheduling} setServicesScheduling={setServicesScheduling} />
+				: <AnySchedulingAdvice page='agendamento' />}
 			<AddSchedulingForm servicesScheduling={servicesScheduling} setServicesScheduling={setServicesScheduling} />
 		</div>
 	)
