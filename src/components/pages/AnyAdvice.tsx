683dd1a1b5e0f566a77ffc4658a4121066091057
Deivid Silva <imdeividsilva@gmail.com>
2023-08-21 15:21:56 -0300

    espaçamento do AnyAdvice

--- src/components/pages/AnyAdvice.tsx
+++ src/components/pages/AnyAdvice.tsx
@@ -6,7 +6,7 @@ const AnyAdvice = ({ page }: Props) => {
 
     return (
         <div>
-            <h6 className='text-center my-4'>Você ainda não criou nenhum {page}</h6>
+            <h6 className='text-center mt-2 mb-4'>Você ainda não criou nenhum {page}</h6>
         </div>
     )
 
