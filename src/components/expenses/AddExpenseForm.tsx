281b17a2c06c82f7004cceef72cc0f389e56510b
Deivid Silva <imdeividsilva@gmail.com>
2023-08-24 14:03:22 -0300

    ajustes no form

--- src/components/expenses/AddExpenseForm.tsx
+++ src/components/expenses/AddExpenseForm.tsx
@@ -1,16 +1,30 @@
+import { useRef } from 'react'
 import { formButtonStyle } from 'commonStyles'
 
 const AddExpenseForm = () => {
+
+	const dateRef = useRef<HTMLInputElement>(null)
+	const nameRef = useRef<HTMLInputElement>(null)
+	const valueRef = useRef<HTMLInputElement>(null)
+
 	return (
 		<form className='d-flex flex-column'>
 			<div className='input-group'>
 				<label className='input-group-text'>Escolha uma data</label>
-				<input className='pe-1 form-control text-center' type='date' required />
+				<input ref={dateRef} className='pe-1 form-control text-center' type='date' required />
 			</div>
-			<input className='form-control text-center p-1 my-3' type='text' placeholder='Digite o nome da despesa' required />
+			<div className='input-group my-3'>
+                <label className='input-group-text' htmlFor='services'>Nome</label>
+                <input ref={nameRef} className='form-control text-center' type='text' required />
+            </div>
+            <div className='input-group mb-3'>
+                <label className='input-group-text' htmlFor='services'>Valor</label>
+                <input ref={valueRef} className='form-control text-center' type='text' required />
+            </div>
 			<button className={formButtonStyle} type='submit'>Registrar Despesa</button>
 		</form>
 	)
+
 }
 
 export default AddExpenseForm
