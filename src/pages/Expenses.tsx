ddec17b06870f9eb15828c77b97d51f53cbc3404
Deivid Silva <imdeividsilva@gmail.com>
2023-08-25 15:50:55 -0300

    setando os expenses ao iniciar

--- src/pages/Expenses.tsx
+++ src/pages/Expenses.tsx
@@ -5,10 +5,20 @@ import { container } from 'commonStyles'
 import ExpensesTable from 'components/expenses/ExpensesTable'
 import AddExpenseForm from 'components/expenses/AddExpenseForm'
 import AnyExpensesAdvice from 'components/pages/AnyAdvice'
+import { SERVER_URL } from 'App'
 
 const Expenses = ({ setCurrentPage }: Props) => {
 
 	const [expenses, setExpenses] = useState<TExpenses>([])
+
+	useEffect(() => {
+		fetch(`${SERVER_URL}/all-expenses`)
+			.then(res => res.json())
+			.then((expenses: TExpenses) => {
+				const orderExpenses = expenses.sort((a, b) => a.date.localeCompare(b.date)).reverse()
+				setExpenses(orderExpenses)
+			})
+	}, [])
 
 	useEffect(() => {
 		setCurrentPage(2)
