c485058df3872878c4a66c82d1998ab28e68a301
Deivid Silva <imdeividsilva@gmail.com>
2023-09-11 16:17:07 -0300

    SchedulingFilterInput - criação do array months

--- src/components/schedulings/SchedulingFilterInput.tsx
+++ src/components/schedulings/SchedulingFilterInput.tsx
@@ -4,20 +4,26 @@ interface SchedulingFilterInputProps {
 
 const SchedulingFilterInput = ({ setSchedulingFilter }: SchedulingFilterInputProps) => {
 
+    const months = [
+        ['01', 'Janeiro'],
+        ['02', 'Fevereiro'],
+        ['03', 'Março'],
+        ['04', 'Abril'],
+        ['05', 'Maio'],
+        ['06', 'Junho'],
+        ['07', 'Julho'],
+        ['08', 'Agosto'],
+        ['09', 'Setembro'],
+        ['10', 'Outubro'],
+        ['11', 'Novembro'],
+        ['12', 'Dezembro']
+    ]
+
     return (
         <select className='mt-2 text-center form-select w-25'>
-            <option value='01'>Janeiro</option>
-            <option value='02'>Fevereiro</option>
-            <option value='03'>Março</option>
-            <option value='04'>Abril</option>
-            <option value='05'>Maio</option>
-            <option value='06'>Junho</option>
-            <option value='07'>Julho</option>
-            <option value='08'>Agosto</option>
-            <option value='09'>Setembro</option>
-            <option value='10'>Outubro</option>
-            <option value='11'>Novembro</option>
-            <option value='12'>Dezembro</option>
+            {months.map(month => {
+                return <option value={month[0]}>{month[1]}</option>
+            })}
         </select>
     )
 
