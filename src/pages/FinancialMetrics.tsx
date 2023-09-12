8d68ff0bb73a6af2058e945347d69777cad8e3f9
Deivid Silva <imdeividsilva@gmail.com>
2023-09-12 19:40:13 -0300

    FinancialMetrics - criação do shape da página

--- src/pages/FinancialMetrics.tsx
+++ src/pages/FinancialMetrics.tsx
@@ -1,15 +1,30 @@
-import { useEffect } from 'react'
+import { useState, useEffect } from 'react'
+
+import { getCurrentMonth } from 'formFunctions/common'
+import { container } from 'commonStyles'
 
 import { Props as FinancialMetricsProps } from 'types/pages'
 
+import MonthInput from 'components/forms/MonthInput'
+
 const FinancialMetrics = ({ setCurrentPage }: FinancialMetricsProps) => {
+
+    const [financialFilter, setFinancialFilter] = useState(getCurrentMonth())
 
     useEffect(() => {
 		setCurrentPage(1)
 	}, [setCurrentPage])
 
     return (
-        <h1>Financial Metrics</h1>
+        <div className='container d-flex flex-column align-items-start'>
+            <MonthInput setTargetFilter={setFinancialFilter} />
+            <ul className='mt-1 list-group'>
+                <li className='list-group-item'>Receita: </li>
+                <li className='list-group-item'>Custo: </li>
+                <li className='list-group-item'>Lucro: </li>
+                <li className='list-group-item'>Margem de Lucro: </li>
+            </ul>
+        </div>
     )
     
 }
