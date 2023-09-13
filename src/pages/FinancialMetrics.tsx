ecd3f0c2598ce2e4e07a65ac92ab600d70d56ac4
Deivid Silva <imdeividsilva@gmail.com>
2023-09-13 16:51:27 -0300

    FinancialMetrics - criação da função getFinancialIncome

--- src/pages/FinancialMetrics.tsx
+++ src/pages/FinancialMetrics.tsx
@@ -1,6 +1,6 @@
 import { useContext, useState, useEffect } from 'react'
 
-import { getCurrentMonth } from 'formFunctions/common'
+import { getCurrentMonth, moneyFormat } from 'formFunctions/common'
 
 import { Props as FinancialMetricsProps } from 'types/pages'
 
@@ -14,6 +14,23 @@ const FinancialMetrics = ({ setCurrentPage }: FinancialMetricsProps) => {
 
     const [financialFilter, setFinancialFilter] = useState(getCurrentMonth())
 
+    const filteredShedulings = () => {
+        return schedulings.filter(scheduling => {
+            return scheduling.date.slice(0, 7) === financialFilter
+        })
+    }
+
+    const getFinancialIncome = () => {
+        return filteredShedulings().reduce((acc, current) => {
+            const values = current.service.value
+            let sum = 0
+            for (const value of values) {
+                sum += value
+            }
+            return acc + sum
+        }, 0)
+    }
+
     useEffect(() => {
 		setCurrentPage(1)
 	}, [setCurrentPage])
@@ -21,8 +38,8 @@ const FinancialMetrics = ({ setCurrentPage }: FinancialMetricsProps) => {
     return (
         <div className='container d-flex flex-column align-items-start'>
             <MonthInput setTargetFilter={setFinancialFilter} />
-            <ul className='mt-1 list-group'>
-                <li className='list-group-item'>Receita: </li>
+            <ul className='mt-2 list-group'>
+                <li className='list-group-item'>Receita: {filteredShedulings()[0] ? moneyFormat(getFinancialIncome()) : null}</li>
                 <li className='list-group-item'>Custo: </li>
                 <li className='list-group-item'>Lucro: </li>
                 <li className='list-group-item'>Margem de Lucro: </li>
