712d468b5aac99d5f0b27a91b6b7c1b771201c32
Deivid Silva <imdeividsilva@gmail.com>
2023-09-16 18:44:15 -0300

    correção de erro de design

--- src/pages/Loading.tsx
+++ src/pages/Loading.tsx
@@ -3,7 +3,7 @@ import { Loader2 } from 'lucide-react'
 const LoadingPage = () => {
 
 	return (
-		<div className='position-absolute vw-100 vh-100 top-0 start-0 bg-white d-flex justify-content-center align-items-center'>
+		<div style={{ zIndex: 1 }} className='position-absolute vw-100 vh-100 top-0 start-0 bg-white d-flex justify-content-center align-items-center'>
 			<Loader2 className='loader' size={50} />
 		</div>
 	)
