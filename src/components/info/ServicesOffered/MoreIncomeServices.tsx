21cc322b56708a403815d7d5f81bc4f0a7a94300
Deivid Silva <imdeividsilva@gmail.com>
2023-09-18 16:49:08 -0300

    MoreIncomeServices - indicando serviços com maiores rendimentos

--- src/components/info/ServicesOffered/MoreIncomeServices.tsx
+++ src/components/info/ServicesOffered/MoreIncomeServices.tsx
@@ -7,11 +7,14 @@ interface MoreIncomeServicesProps {
 const MoreIncomeServices = ({ services }: MoreIncomeServicesProps) => {
 
     return (
-        <ol className='mb-0'>
-            {services.map(service => {
-                return <li><strong>{service[0]}:</strong> {moneyFormat(service[1])}</li>
-            })}
-        </ol>
+        <div>
+            <h5 className='mb-2'>Serviços com maiores rendimentos:</h5>
+            <ol className='mb-0'>
+                {services.map(service => {
+                    return <li className='ms-0'><strong>{service[0]}:</strong> {moneyFormat(service[1])}</li>
+                })}
+            </ol>
+        </div>
     )
 
 }
