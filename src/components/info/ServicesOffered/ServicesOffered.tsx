036d9a3fedcdc2b102c74c166c559b1d9f113ad8
Deivid Silva <imdeividsilva@gmail.com>
2023-09-18 14:32:19 -0300

    ServicesOferred - criação da função getMoreIncomeServices

--- src/components/info/ServicesOffered/ServicesOffered.tsx
+++ src/components/info/ServicesOffered/ServicesOffered.tsx
@@ -2,14 +2,14 @@ import { ServiceSchedulings } from 'types/schedulings'
 
 import ServicesChart from './ServicesChart'
 
-interface ServicesOfferedProps {
+interface OfferedServicesProps {
     schedulings: ServiceSchedulings
     setShowServicesOffered: React.Dispatch<React.SetStateAction<boolean>>
 }
 
-const ServicesOffered = ({ schedulings, setShowServicesOffered }: ServicesOfferedProps) => {
+const OfferedServices = ({ schedulings, setShowServicesOffered }: OfferedServicesProps) => {
 
-    const getMostOfferServices = () => {
+    const getMostOfferedServices = () => {
         const servicesArrays = schedulings.map(scheduling => {
             return scheduling.service.name
         }).flat()
@@ -25,13 +25,46 @@ const ServicesOffered = ({ schedulings, setShowServicesOffered }: ServicesOffere
         return services
     }
 
+    const getMoreIncomeServices = () => {
+
+        // Obtendo os services prestados no mês
+        const servicesArrays = schedulings.map(scheduling => {
+            return scheduling.service.name
+        }).flat()
+
+        // Array dos dados da função
+        const services: [string, number][] = Array.from(new Set(servicesArrays))
+        .map(service => [service, 0])
+        
+        // Criação do formato que será utilizado para incrementar os valores
+        const newFormat: [string[], number[]][] = schedulings.map(scheduling => {
+            return [scheduling.service.name, scheduling.service.value]
+        })
+        
+        // Incrementando os valores rendidos de cada serviço
+        newFormat.forEach(current => {
+            current[0].forEach((serviceName, index) => {
+                for (const service of services) {
+                    if (serviceName === service[0]) {
+                        service[1] += current[1][index]
+                    }
+                }
+            })
+        })
+
+        return services.sort((a, b) => {
+            return b[1] - a[1]
+        })
+        
+    }
+
     return (
         <div className='w-100'>
-            <ServicesChart services={getMostOfferServices()} />
+            <ServicesChart services={getMostOfferedServices()} />
             <button onClick={() => setShowServicesOffered(false)} className='btn btn-sm btn-primary'>Rendimentos</button>
         </div>
     )
 
 }
 
-export default ServicesOffered
+export default OfferedServices
